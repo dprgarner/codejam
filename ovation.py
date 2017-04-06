df47b2d50312593377e0e8e85c74d20e30c54f52
David Garner <dprgarner@gmail.com>
2017-04-06 23:41:17 +0100

    Codejam helper class, with 2015 A qualification round solution

---
+++ ovation.py
@@ -0,0 +1,25 @@
+from codejam import CodeJamParser
+
+
+class Ovation(CodeJamParser):
+    def get_cases(self):
+        cases = int(next(self.source))
+        for i in range(1, cases + 1):
+            case_line = next(self.source)
+            _, shynesses_str = case_line.split(' ')
+            yield [int(c) for c in list(shynesses_str)],
+
+    def handle_case(self, shyness_count):
+        partial_sums = [
+            sum(shyness_count[:i])
+            for i in range(1, len(shyness_count) + 1)
+        ]
+
+        return max([
+            1 + i - partial_sums[i]
+            for i in range(len(partial_sums))
+        ] + [0])
+
+
+if __name__ == '__main__':
+    Ovation()
