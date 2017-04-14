0950fc7e627105168a803ad46b4aec59ace24ac3
David Garner <dprgarner@gmail.com>
2017-04-14 12:21:41 +0100

    Attempting to make the code less confusing with some comments and variable-renamings.

--- python/bffs.py
+++ python/bffs.py
@@ -4,6 +4,8 @@ class BFFs(CodeJamParser):
     """
     Q3, Round 1A, 2016 (Practice)
     https://code.google.com/codejam/contest/4304486/dashboard#s=p2
+
+    ... it's not the prettiest of code, but it works.
     """
     def get_cases(self):
         cases = int(next(self.source))
@@ -19,9 +21,9 @@ class BFFs(CodeJamParser):
         for i, dest in enumerate(bffs):
             semiperm[i+1] = dest
 
-        # First, find the largest cycle. Then, find largest path ending in 2-cycle.
-
-        # Assign to this when we find the largest possible cycle for that entry.
+        # First, find the largest cycle.
+        # Assign k:v to this when we find whether k is in a cycle of size v.
+        # Assign k: -1 if k is not in a cycle.
         largest_cycles = {}
 
         for index in range(1, n + 1):
@@ -31,7 +33,7 @@ class BFFs(CodeJamParser):
 
             while index not in cycle:
                 if largest_cycles.get(index):
-                    # None of the entries in the list form a cycle here.
+                    # None of the entries in the list form a cycle.
                     for i in cycle:
                         largest_cycles[i] = -1
                     break
@@ -41,7 +43,7 @@ class BFFs(CodeJamParser):
             if largest_cycles.get(index):
                 continue
 
-            # Numbers to right are in cycle, numbers left are not.
+            # Numbers to right are in the cycle, numbers to the left are not.
             cutoff_index_for_cycle = cycle.index(index)
             for i in cycle[:cutoff_index_for_cycle]:
                 largest_cycles[i] = -1
@@ -51,17 +53,18 @@ class BFFs(CodeJamParser):
 
         largest_cycle = max(largest_cycles.values())
 
+        # Next, find paths which end in a 2-cycle. Also record the 2-cycle
+        # indices at the end of these paths.
         largest_paths = {}
         potential_path_starts = [k for k, v in largest_cycles.items() if v == -1]
         destination_2_cycles = {}
 
-        # Find paths ending in 2-cycle
         for start in potential_path_starts:
             path = []
             index = start
             while largest_cycles.get(index) != 2:
                 if largest_cycles.get(index) > 2:
-                    # Not a valid path.
+                    # The path does not end in a 2-cycle.
                     for i in path:
                         largest_paths[i] = -1
                     break
@@ -81,9 +84,11 @@ class BFFs(CodeJamParser):
             if largest_cycles[a] == 2
             and a < semiperm[a]
         ]
-
-        # Find the length of all max paths that end in each side of the 2-cycle.
-        ending_in_2_cycle_max = 0
+        # Find the maximum length of the paths which end at each side of the
+        # 2-cycle, to find the largest 'double-path' ending in a 2-cycle at
+        # both ends. Sum these all together to find the largest total size of
+        # the circle when all the paths are 2-cycle paths.
+        ending_in_2_cycles_total = 0
         for (i, j) in two_cycles:
             ending_in_i = [
                 x for x, y in destination_2_cycles.items() if y == i
@@ -97,59 +102,10 @@ class BFFs(CodeJamParser):
                 max_i_path = max(max_i_path, largest_paths[x])
             for x in ending_in_j:
                 max_j_path = max(max_j_path, largest_paths[x])
-            # print('ending_in_i', ending_in_i)
-            # print('ending_in_j', ending_in_j)
-            # print('max_i_path', max_i_path)
-            # print('max_j_path', max_j_path)
             length_of_2_cycle_path = 2 + max_i_path + max_j_path
-            ending_in_2_cycle_max += length_of_2_cycle_path
-        # print (bffs)
-        # print (semiperm)
-        # print('largest_cycles:', largest_cycles)
-        # print('largest_paths:', largest_paths)
-        # print('destination_2_cycles', destination_2_cycles)
-        # print('ending_in_2_cycle_max', ending_in_2_cycle_max)
-        return max(largest_cycle, ending_in_2_cycle_max)
-
-
+            ending_in_2_cycles_total += length_of_2_cycle_path
+        return max(largest_cycle, ending_in_2_cycles_total)
 
 
 if __name__ == '__main__':
     BFFs()
-
-
-
-
-# while index not in cycle:
-#     if largest_cycles.get(index):
-#         break
-#     cycle.append(index)
-#     index = semiperm.get(index)
-# closed_cycle = bool(largest_cycles.get(index))
-
-# for i in cycle:
-#     largest_cycles[i] = (
-#         len(cycle)
-#         if closed_cycle
-#         else largest_cycles.get(i, -1)
-#     )
-
-
-
-
-
-
-# for i in range(n):
-#     if not semiperm:
-#         break
-#     index = i + 1
-#     if not semiperm.get(index):
-#         continue
-
-#     start = index
-#     cycle_size = 0
-#     while index != start:
-#         index = semiperm.get(index)
-#         cycle_size += 1
-#     if index == start:
-#         max_cycle = max(max_cycle, cycle_size)
