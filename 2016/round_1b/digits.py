228ffed9822f76b2a3aabb1b033341a4874ad086
David Garner <dprgarner@gmail.com>
2021-07-03 13:19:55 +0100

    Digits but in JS

--- 2016/round_1b/digits.py
+++ 2016/round_1b/digits.py
@@ -1,53 +1,60 @@
-from codejam import CodeJamParser
+"""
+2017, Qualification round, A
+https://code.google.com/codejam/contest/3264486/dashboard
+"""
 
-DIGITS = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"]
+DIGITS = [
+    "ZERO",
+    "ONE",
+    "TWO",
+    "THREE",
+    "FOUR",
+    "FIVE",
+    "SIX",
+    "SEVEN",
+    "EIGHT",
+    "NINE",
+]
 
 LEVELS = [
     {
-        'Z': 0,
-        'X': 6,
-        'G': 8,
+        "Z": 0,
+        "W": 2,
+        "U": 4,
+        "X": 6,
+        "G": 8,
     },
     {
-        'W': 2,
-        'H': 3,
-        'U': 4,
-        'S': 7,
+        "O": 1,
+        "H": 3,
+        "F": 5,
+        "S": 7,
     },
     {
-        'O': 1,
-        'F': 5,
-    },
-    {
-        'I': 9,
+        "I": 9,
     },
 ]
 
 
-class Digits(CodeJamParser):
-    """
-    2017, Qualification round, A
-    https://code.google.com/codejam/contest/3264486/dashboard
-    """
-    def get_cases(self):
-        cases = int(next(self.source))
-        for i in range(1, cases + 1):
-            case_line = next(self.source)
-            yield sorted(case_line),
-
-    def handle_case(self, phone_letters):
-        numbers = []
-        for level in LEVELS:
-            for letter in level.keys():
-                occurences = phone_letters.count(letter)
-                number = level[letter]
-                for _ in range(occurences):
-                    numbers.append(number)
-                    for x in DIGITS[number]:
-                        phone_letters.remove(x)
-        return ''.join(str(s) for s in sorted(numbers))
-    
-if __name__ == '__main__':
-    Digits()
+def solve_case(phone_letters):
+    numbers = []
+    for level in LEVELS:
+        for letter in level.keys():
+            occurences = phone_letters.count(letter)
+            number = level[letter]
+            for _ in range(occurences):
+                numbers.append(number)
+                for x in DIGITS[number]:
+                    phone_letters.remove(x)
+    return "".join(str(s) for s in sorted(numbers))
 
 
+def run():
+    cases = int(input())
+    for i in range(1, cases + 1):
+        soln = solve_case(list(input()))
+        print("Case #{}: {}".format(i, soln), flush=True)
+
+
+if __name__ == "__main__":
+    run()
